5df88244b5cff473984fc57eee0dd0992cf3ca2e
Omarmoatz <omar.raouf.002@gmail.com>
2024-02-02 16:05:00 +0200

    design models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=100, blank=True, default="")
+    note = models.TextField(max_length=1000, blank=True, default="")
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task
